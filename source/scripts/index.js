4268a22c69287f98eb65f78a94dbf37772047863
NovikoffArtem <142929498+NovikoffArtem@users.noreply.github.com>
2024-10-27 23:23:27 +0300

    slider

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,36 @@
-/* в этот файл добавляет скрипты*/
+import '../vendor/nouislider.min.js';
+
+const rangeSlider = document.querySelector('.price__range-slider');
+
+if (rangeSlider) {
+  noUiSlider.create(rangeSlider, {
+    start: [0, 900],
+    connect: true,
+    step: 1,
+    range: {
+      'min': 0,
+      'max': 900
+    },
+    cssPrefix: 'noui-'
+  });
+
+  const inputLow = document.querySelector('.price__field--low');
+  const inputHigh = document.querySelector('.price__field--high');
+  const inputsArray = [inputLow, inputHigh];
+
+  rangeSlider.noUiSlider.on('update', function (values, handle) {
+    inputsArray[handle].value = Math.round(values[handle]);
+  });
+
+  const setRangeSlider = (i, value) => {
+    const arr = [null, null];
+    arr[i] = value;
+    rangeSlider.noUiSlider.set(arr);
+  };
+
+  inputsArray.forEach((el, index) => {
+    el.addEventListener('change', (e) => {
+      setRangeSlider(index, e.currentTarget.value);
+    });
+  });
+}
